29364fc8a4776e6444eb4dbea6cafae48f8ed645
imon <imon.ilannoor@gmail.com>
2023-12-06 16:51:37 +0600

    # Environment Setup

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ use App\Http\Controllers\Blog\PageController;
 use App\Http\Controllers\Blog\SubscriptionController;
 use App\Http\Controllers\Blog\SitemapController;
 /*
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
