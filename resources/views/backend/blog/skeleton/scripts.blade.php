6da77409f7307d558872f1c10607c99ebfe93a21
imon <imon.ilannoor@gmail.com>
2023-12-30 16:34:14 +0600

    # Initial Migration # TinyMCE Domain Approving Issue Solved

--- resources/views/backend/blog/skeleton/scripts.blade.php
+++ resources/views/backend/blog/skeleton/scripts.blade.php
@@ -1,2 +1,3 @@
 	<!-- JSScript -->
+	<script src="https://cdn.tiny.cloud/1/7g6ltyz3lnm9kzavr5tcbpgw1w9rtu2cplxmquh4ztvb53aa/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 	@vite(['resources/js/bootstrap.bundle.min.js'])
